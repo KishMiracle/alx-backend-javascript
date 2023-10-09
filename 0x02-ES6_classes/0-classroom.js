96866acbeb651c76f3d4cb95842915d539aef532
KishMiracle <miraclekish@gmail.com>
2023-10-09 01:30:36 +0100

    9-hoisting.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,6 @@
-/*Java script*/
+/*Java script classroom from file 0-classroom.js*/
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
+    this._maxStudentsSize = maxStudentsSize; // this will display sizes
   }
 }
